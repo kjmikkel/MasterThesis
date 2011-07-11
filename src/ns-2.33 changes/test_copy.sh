4dc1a42247163d09e7062e1ea30b759407de6a3d
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2011-07-12 01:51:33 +0200

    the copy functions have been updated to better fit the other changes

--- src/ns-2.33 changes/test_copy.sh
+++ src/ns-2.33 changes/test_copy.sh
@@ -2,13 +2,10 @@
 
 NS_ROOT="../../ns-allinone-2.33/ns-2.33/"
 
-cp Makefile $NS_ROOT"/."
-
-cp packet.h $NS_ROOT"/common/."
-
 # The test folder is copied
 cp -rf goafr_test $NS_ROOT
 
 # Move to directory and run test
 cd ../../ns-allinone-2.33/ns-2.33/goafr_test/
+
 ../ns goafr_test_00.tcl
