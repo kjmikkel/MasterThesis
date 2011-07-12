30ecfc148f26d101b84718d1e9073b49b0babbe9
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2011-07-12 08:39:24 +0200

    better use of variables

--- src/ns-2.33 changes/test_copy.sh
+++ src/ns-2.33 changes/test_copy.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 NS_ROOT="../../ns-allinone-2.33/ns-2.33/"
+SRC_FOLDER="../../../src/ns-2.33 changes/"
 
 # The test folder is copied
 cp -rf goafr_test $NS_ROOT
 
 # Move to directory and run test
-cd ../../ns-allinone-2.33/ns-2.33/goafr_test/
+cd $NS_ROOT"goafr_test/"
 
 ../ns goafr_test_00.tcl
+cp goafr_test.tr $SRC_FOLDER"goafr_test/."
+cp goafr_00.nam $SRC_FOLDER"goafr_test/."
