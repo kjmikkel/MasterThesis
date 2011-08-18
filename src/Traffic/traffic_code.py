a864688ad05bc993fc9cf1bfdcd279b1644e778f
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2011-08-18 22:10:30 +0200

    the final traffic code

--- src/Traffic/traffic_code.py
+++ src/Traffic/traffic_code.py
@@ -1,10 +1,22 @@
-import os
+import os, re
+
+"""
+os.chdir("Trace")
+files = os.listdir(os.getcwd())
+m = re.compile("(\d+)")
+for f in files:
+  res = m.search(f)
+  new_name = "Traffic-%s-1.tcl" % (res.group(1))
+  os.system("mv %s %s" % (f, new_name))
+"""
 
 os.chdir("../../ns-allinone-2.33/ns-2.33/indep-utils/cmu-scen-gen/")
 
-for i in xrange(100):
-  nodes = 100 * (i + 1)
-  name = "Traffic-%s.tcl" % nodes
-  third = nodes / 3
-  os.system("../../ns cbrgen.tcl -type tcp -nn %s -seed -mc %s -max 60 > %s" % (nodes, third, name))
-  os.system("mv %s ../../../../src/Traffic/Trace/%s" % (name, name))
+for j in xrange(2):
+  for i in xrange(100):
+    nodes = 10 * (i + 1)
+    name = "Traffic-%s-%s.tcl" % (nodes, j)
+    third = nodes / 3
+    os.system("../../ns cbrgen.tcl -type tcp -nn %s -seed -mc %s -max 60 > %s" % (nodes, third, name))
+    os.system("mv %s ../../../../src/Traffic/Trace/%s" % (name, name))
+
